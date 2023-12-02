298c681d35364a0043f1d15a6b84b1dc390050d5
Mehedi Hasan <couict6@gmail.com>
2023-12-02 16:48:43 +0600

    mdevice

--- src/components/counterapp/counter.jsx
+++ src/components/counterapp/counter.jsx
@@ -1,6 +1,6 @@
 import {useDispatch, useSelector} from "react-redux";
 import {decrement, increment, reset, setCustomValue} from "../../redux/state/counter/counterSlice.js";
-import {useRef} from "react";
+import React, {useRef} from "react";
 
 
 const Counter = () => {
@@ -11,25 +11,32 @@ const Counter = () => {
 
 
 
-    return <div className="card">
-        <div className="card-header bg-secondary">
-            <h4 className="text-white text-center">My counter App</h4>
-        </div>
-        <div className="card-body">
-            <h4 className="text-secondary">{count}</h4>
+    return(
+    <div className="container-fluid">
+        <div className="row d-flex justify-content-center">
+            <div className="col-md-12">
+                <div className="card">
+                    <div className="card-header bg-secondary">
+                        <h4 className="text-white text-center">My counter App</h4>
+                    </div>
+                    <div className="card-body">
+                        <h4 className="text-secondary">{count}</h4>
 
-            <div className="my-4">
-                <button onClick={()=>dispacth(increment())} className="btn btn-success">Increase</button>
-                <button onClick={()=>dispacth(decrement())} className="btn mx-2 btn-danger">Decrease</button>
-            </div>
-            <div className="my-4">
-                <input ref={num} className="form-control w-50 my-2 " type="number" placeholder="Enter a customize value..."/>
-                <button onClick={()=>dispacth(setCustomValue(num.current.value))} className="btn btn-danger mx-1 w-25 my-2">custom set</button>
-                <button onClick={()=>dispacth(reset())} className="btn mx-1 w-25 btn-secondary">Reset</button>
+                        <div className="my-4">
+                            <button onClick={()=>dispacth(increment())} className="btn btn-success">Increase</button>
+                            <button onClick={()=>dispacth(decrement())} className="btn mx-2 btn-danger">Decrease</button>
+                        </div>
+                        <div className="my-4">
+                            <input ref={num} className="form-control w-50 my-2 " type="number" placeholder="Enter a customize value..."/>
+                            <button onClick={()=>dispacth(setCustomValue(num.current.value))} className="btn btn-danger mx-1 w-25 my-2">custom set</button>
+                            <button onClick={()=>dispacth(reset())} className="btn mx-1 w-25 btn-secondary">Reset</button>
+                        </div>
+                    </div>
+
+                </div>;
             </div>
         </div>
-
-    </div>;
-};
+    </div>
+    )};
 
 export default Counter;
